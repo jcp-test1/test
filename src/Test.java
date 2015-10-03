6a7cd2e5b52088e3409e3887877d14a675ba041b
TestUser1 <jesus.chicharro.perez@gmail.com>
2015-10-03 08:33:29 +0200

    i

--- src/Test.java
+++ src/Test.java
@@ -3,4 +3,12 @@ public class Test {
     public static void main(String [] a) {
         System.out.println("Mixing Python with Java!");
     }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
 }
